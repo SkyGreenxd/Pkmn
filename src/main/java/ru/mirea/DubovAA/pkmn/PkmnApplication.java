1cef1e9f41c4bd187f65a219e9c3d61a3f109608
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2024-10-01 22:20:24 +0300

    пуп

--- src/main/java/ru/mirea/DubovAA/pkmn/PkmnApplication.java
+++ src/main/java/ru/mirea/DubovAA/pkmn/PkmnApplication.java
@@ -6,7 +6,7 @@ public class PkmnApplication {
     public static void main(String[] args) throws ClassNotFoundException, IOException {
         Card myPokemonCard = CardImport.impFromTxt("src/main/resources/my_card.txt");
         System.out.println(myPokemonCard);
-        CardExport.сardExport(myPokemonCard);
+        CardExport.cardExport(myPokemonCard);
 
         Card import_some_card = CardImport.cardImport("src/main/resources/Durant.crd");
         System.out.println(import_some_card);
