b4ec85c593c13357a909973bb079570a9d7f4025
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2024-11-07 17:48:08 +0300

    так ну я че та сделал

--- src/main/java/ru/mirea/pkmn/DubovAA/PkmnApplication.java
+++ src/main/java/ru/mirea/pkmn/DubovAA/PkmnApplication.java
@@ -1,25 +1,27 @@
 package ru.mirea.pkmn.DubovAA;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import ru.mirea.pkmn.Card;
 import ru.mirea.pkmn.DubovAA.web.http.PkmnHttpClient;
 import ru.mirea.pkmn.DubovAA.web.jdbc.DatabaseServiceImpl;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.stream.Collectors;
 
 public class PkmnApplication {
     public static final long serialVersionUID = 1L;
-    public static void main(String[] args) throws IOException, SQLException {
+    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
         PkmnHttpClient httpClient = new PkmnHttpClient();
-        DatabaseServiceImpl db = new DatabaseServiceImpl();
+//        DatabaseServiceImpl db = new DatabaseServiceImpl();
 
         Card myPokemonCard = CardImport.impFromTxt("src/main/resources/my_card.txt");
         CardImport.AttackSkillUpd(myPokemonCard, httpClient);
 
-        db.createPokemonOwner(myPokemonCard.getPokemonOwner());
-        db.saveCardToDatabase(myPokemonCard);
+//        db.createPokemonOwner(myPokemonCard.getPokemonOwner());
+//        db.saveCardToDatabase(myPokemonCard);
 
 
-/*        System.out.println("Моя карточка: " + myPokemonCard);
+       System.out.println("Моя карточка: " + myPokemonCard);
         CardExport.cardExport(myPokemonCard);
 
         Card import_some_card = CardImport.cardImport("src/main/resources/Pyroar.crd");
@@ -34,6 +36,6 @@ public class PkmnApplication {
         System.out.println(card.findValues("attacks")
                 .stream()
                 .map(JsonNode::toPrettyString)
-                .collect(Collectors.toSet()));*/
+                .collect(Collectors.toSet()));
     }
 }
